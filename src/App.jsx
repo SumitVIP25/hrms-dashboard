ec691b4ef79751e1d5e8f3dde70c3f5f10e7c1bf
SumitVIP25 <prashantsumit08@gmail.com>
2026-05-19 21:49:06 +0530

    Initial HRMS project setup

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Employees from "./pages/Employees";
+import Login from "./pages/Login";
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          <Route path='/' element={<Login />} />
+          <Route path='dashboard' element={<Dashboard />} />
+          <Route path='/employees' element={<Employees />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App
