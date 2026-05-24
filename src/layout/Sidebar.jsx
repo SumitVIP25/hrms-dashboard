181b781500ca904091ee1187d2966c2f6253fdac
SumitVIP25 <prashantsumit08@gmail.com>
2026-05-24 20:45:57 +0530

    Created sidebar navigation links

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+
 export default function Sidebar() {
     return (
         <div className="sidebar bg-dark text-white p-3"
@@ -5,10 +7,10 @@ export default function Sidebar() {
         >
             <h4 className="mb-4">HRMS</h4>
             <ul className="list-unstyled">
-                <li className="mb-3">Dashboard</li>
-                <li className="mb-3">Employees</li>
-                <li className="mb-3">Attendance</li>
-                <li className="mb-3">Leaves</li>
+                <li className="mb-3"><NavLink to="/dashboard" className={({ isActive }) => isActive ? "text-warning text-decoration-none" : "text-white text-decoration-none"}>Dashboard</NavLink></li>
+                <li className="mb-3"> <NavLink to="/employees" className={({ isActive }) => isActive ? "text-warning text-decoration-none" : "text-white text-decoration-none"}>Employees</NavLink></li>
+                <li className="mb-3"> <NavLink to="/attendance" className="text-white text-decoration-none">Attendance</NavLink></li>
+                <li className="mb-3"><NavLink to="/leaves" className="text-white text-decoration-none">Leaves</NavLink></li>
             </ul>
         </div >);
 }
