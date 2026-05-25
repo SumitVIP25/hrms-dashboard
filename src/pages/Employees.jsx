27f89ab67cd090f0b5cf7f4e2d176e1f7af5ea00
SumitVIP25 <prashantsumit08@gmail.com>
2026-05-25 22:07:03 +0530

    Add add employee functionality

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -6,6 +6,9 @@ export default function Employees() {
     const [employees, setEmployees] = useState([]);
     const [loading, setLoading] = useState(true);
     const [search, setSearch] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+
 
     const handelDelete = (id) => {
         const updatedEmployees = employees.filter((employee) => employee.id !== id);
@@ -32,10 +35,56 @@ export default function Employees() {
         return <h2>Loading...</h2>;
     }
 
+    const handleAddEmployee = () => {
+
+        if (name.trim() === "" || email.trim() === "") {
+            alert("Please fill all fields");
+            return;
+        }
+
+        const newEmployee = {
+            id: employees.length + 1,
+            name: name,
+            email: email,
+        };
+        setEmployees([...employees, newEmployee]);
+        setName("");
+        setEmail("");
+    }
+
     return (
         <MainLayout>
             <div>
                 <h3 className="mb-4">Employees List</h3>
+
+                <div className="card p-3 mb-4">
+                    <h4>Add Employee</h4>
+                    <div className="row g-3">
+                        <div className="col-md-5">
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Enter name"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                            />
+                        </div>
+
+                        <div className="col-md-5">
+                            <input
+                                type="email"
+                                className="form-control"
+                                placeholder="Enter email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
+                        <div className="col-md-2">
+                            <button className="btn btn-primary w-100" onClick={handleAddEmployee}>Add</button>
+                        </div>
+                    </div>
+                </div>
+
                 <div className="mb-3">
                     <input
                         type="text"
@@ -51,7 +100,7 @@ export default function Employees() {
                             <th>ID</th>
                             <th>Name</th>
                             <th>Email</th>
-                            <th>Department</th>
+                            <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -60,7 +109,6 @@ export default function Employees() {
                                 <td>{employee.id}</td>
                                 <td>{employee.name}</td>
                                 <td>{employee.email}</td>
-                                <td>{employee.department}</td>
                                 <td><button className="btn btn-danger btn-sm" onClick={() => handelDelete(employee.id)}>Delete</button></td>
                             </tr>
                         ))}
